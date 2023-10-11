7c41e8cd80b6ee1db5201624ff23239e38b219d2
VIPIN BOHRA <vipinbohra321@gmail.com>
2023-10-11 14:03:11 +0530

    feat: fixed save channel issue

--- src/lib/modules/firebase/channelsQueue.ts
+++ src/lib/modules/firebase/channelsQueue.ts
@@ -8,8 +8,9 @@ export const queueCreateChannel = async (channelDetails: QueuedChannel) => {
   const db = admin.database();
   const eventsRef = db.ref(`app/createChannelQueue/`);
   try {
-    const ref = eventsRef.push(removeNullKeys(channelDetails));
-    console.log('channel queued', ref.key, removeNullKeys(channelDetails));
+    const newRef = eventsRef.push();
+    await newRef.set(removeNullKeys(channelDetails));
+    console.log('channel queued', newRef.key);
   } catch (error) {
     console.log(error, 'Error while created channel queued');
   }
